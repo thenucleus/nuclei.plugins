0ad14ee8fce758bd15df4f4910cece0167240954
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-09-18 22:40:15 +1200

    Introducing the transport and transport.http projects

--- src/Nuclei.Plugins.Transport/MirroringRepository.cs
+++ src/Nuclei.Plugins.Transport/MirroringRepository.cs
@@ -1,20 +1,23 @@
-﻿//-----------------------------------------------------------------------
-// <copyright company="TheNucleus">
-// Copyright (c) TheNucleus. All rights reserved.
-// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
-// </copyright>
-//-----------------------------------------------------------------------
-
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nuclei.Plugins.Core;
 
-namespace Nuclei.Plugins.Core
+namespace Nuclei.Plugins.Transport
 {
     /// <summary>
-    /// Defines the interface for objects that handle plugin requests.
+    /// Defines an <see cref="IPluginRepository"/> that mirrors another <see cref="IPluginRepository"/>
     /// </summary>
-    public interface ISatisfyPluginRequests
+    public sealed class MirroringRepository : ISatisfyPluginRequests
     {
+
+        public MirroringRepository(IMirrorChannel channel)
+        {
+        }
+
         /// <summary>
         /// Returns a value indicating if the repository contains a <see cref="TypeDefinition"/>
         /// for the given type.
@@ -28,7 +31,9 @@ namespace Nuclei.Plugins.Core
             "Microsoft.StyleCop.CSharp.DocumentationRules",
             "SA1628:DocumentationTextMustBeginWithACapitalLetter",
             Justification = "Documentation can start with a language keyword")]
-        bool ContainsDefinitionForType(string fullyQualifiedName);
+        public bool ContainsDefinitionForType(string fullyQualifiedName)
+        {
+        }
 
         /// <summary>
         /// Returns a value indicating if the repository contains a <see cref="TypeDefinition"/>
@@ -43,14 +48,18 @@ namespace Nuclei.Plugins.Core
             "Microsoft.StyleCop.CSharp.DocumentationRules",
             "SA1628:DocumentationTextMustBeginWithACapitalLetter",
             Justification = "Documentation can start with a language keyword")]
-        bool ContainsDefinitionForType(TypeIdentity type);
+        public bool ContainsDefinitionForType(TypeIdentity type)
+        {
+        }
 
         /// <summary>
         /// Returns the identity for the type given by the name.
         /// </summary>
         /// <param name="fullyQualifiedName">The fully qualified name of the type.</param>
         /// <returns>The requested type.</returns>
-        TypeIdentity IdentityByName(string fullyQualifiedName);
+        public TypeIdentity IdentityByName(string fullyQualifiedName)
+        {
+        }
 
         /// <summary>
         /// Returns a value indicating whether the given <c>child</c> type is derived from the given <c>parent</c> type.
@@ -64,33 +73,43 @@ namespace Nuclei.Plugins.Core
             "Microsoft.StyleCop.CSharp.DocumentationRules",
             "SA1628:DocumentationTextMustBeginWithACapitalLetter",
             Justification = "Documentation can start with a language keyword")]
-        bool IsSubTypeOf(TypeIdentity parent, TypeIdentity child);
+        public bool IsSubTypeOf(TypeIdentity parent, TypeIdentity child)
+        {
+        }
 
         /// <summary>
         /// Returns the part that has the given type as declaring type.
         /// </summary>
         /// <param name="type">The declaring type.</param>
         /// <returns>The requested part.</returns>
-        PartDefinition Part(TypeIdentity type);
+        public PartDefinition Part(TypeIdentity type)
+        {
+        }
 
         /// <summary>
         /// Returns a collection containing all known parts.
         /// </summary>
         /// <returns>The collection containing all known parts.</returns>
-        IEnumerable<PartDefinition> Parts();
+        public IEnumerable<PartDefinition> Parts()
+        {
+        }
 
         /// <summary>
         /// Returns the <see cref="TypeDefinition"/> for the given type.
         /// </summary>
         /// <param name="type">The type.</param>
         /// <returns>The requested type definition.</returns>
-        TypeDefinition TypeByIdentity(TypeIdentity type);
+        public TypeDefinition TypeByIdentity(TypeIdentity type)
+        {
+        }
 
         /// <summary>
         /// Returns the <see cref="TypeDefinition"/> for the type with the given name.
         /// </summary>
         /// <param name="fullyQualifiedName">The fully qualified name for the type.</param>
         /// <returns>The requested type definition.</returns>
-        TypeDefinition TypeByName(string fullyQualifiedName);
+        public TypeDefinition TypeByName(string fullyQualifiedName)
+        {
+        }
     }
 }
