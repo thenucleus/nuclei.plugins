5af23128a70dd0368f437916f00fe4139e759b05
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-10-22 23:58:39 +1300

    Updating the PluginOrigin class to be able to store more identifiying information
    references #2

--- src/Nuclei.Plugins.Discovery/Origin/FileSystem/PluginFileOrigin.cs
+++ src/Nuclei.Plugins.Discovery/Origin/FileSystem/PluginFileOrigin.cs
@@ -6,7 +6,10 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Diagnostics;
+using System.Globalization;
 using Nuclei.Plugins.Core;
+using Nuclei.Plugins.Discovery.Properties;
 
 namespace Nuclei.Plugins.Discovery.Origin.FileSystem
 {
@@ -17,16 +20,11 @@ namespace Nuclei.Plugins.Discovery.Origin.FileSystem
     public sealed class PluginFileOrigin : PluginOrigin
     {
         /// <summary>
-        /// The last time, in coordinated universal time (UTC), that the plugin file was modified.
-        /// </summary>
-        private readonly DateTimeOffset _lastWriteTimeUtc;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="PluginFileOrigin"/> class.
         /// </summary>
         /// <param name="path">The full path to the plugin file.</param>
         public PluginFileOrigin(string path)
-            : base(path)
+            : base(new PluginFileOriginData(path, DateTimeOffset.MinValue, DateTimeOffset.MinValue))
         {
         }
 
@@ -34,11 +32,11 @@ namespace Nuclei.Plugins.Discovery.Origin.FileSystem
         /// Initializes a new instance of the <see cref="PluginFileOrigin"/> class.
         /// </summary>
         /// <param name="path">The full path to the plugin file.</param>
-        /// <param name="lastWriteTimeUtc">The last time the assembly was changed.</param>
-        public PluginFileOrigin(string path, DateTimeOffset lastWriteTimeUtc)
-            : this(path)
+        /// <param name="creationTimeUtc">The time the file was created.</param>
+        /// <param name="lastWriteTimeUtc">The last time the file was changed.</param>
+        public PluginFileOrigin(string path, DateTimeOffset creationTimeUtc, DateTimeOffset lastWriteTimeUtc)
+            : base(new PluginFileOriginData(path, creationTimeUtc, lastWriteTimeUtc))
         {
-            _lastWriteTimeUtc = lastWriteTimeUtc;
         }
 
         /// <summary>
@@ -48,9 +46,23 @@ namespace Nuclei.Plugins.Discovery.Origin.FileSystem
         /// <returns>
         /// A copy of the current ID number.
         /// </returns>
-        protected override PluginOrigin Clone(string value)
+        protected override PluginOrigin Clone(PluginOriginData value)
         {
-            return new PluginFileOrigin(value);
+            var fileOriginData = value as PluginFileOriginData;
+            Debug.Assert(fileOriginData != null, "The internal value should be a PluginFileOriginData instance.");
+
+            return new PluginFileOrigin(fileOriginData.FilePath, fileOriginData.CreationTimeUtc, fileOriginData.LastWriteTimeUtc);
+        }
+
+        /// <summary>
+        /// Gets the time the file was created.
+        /// </summary>
+        public DateTimeOffset CreationTimeUtc
+        {
+            get
+            {
+                return OriginData.CreationTimeUtc;
+            }
         }
 
         /// <summary>
@@ -60,7 +72,7 @@ namespace Nuclei.Plugins.Discovery.Origin.FileSystem
         {
             get
             {
-                return InternalValue;
+                return OriginData.FilePath;
             }
         }
 
@@ -71,7 +83,234 @@ namespace Nuclei.Plugins.Discovery.Origin.FileSystem
         {
             get
             {
-                return _lastWriteTimeUtc;
+                return OriginData.LastWriteTimeUtc;
+            }
+        }
+
+        private PluginFileOriginData OriginData
+        {
+            get
+            {
+                return InternalValue as PluginFileOriginData;
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="string"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "Plugin file {0}  was created at: {1} and last modified at: {2})",
+                OriginData.FilePath,
+                OriginData.CreationTimeUtc,
+                OriginData.LastWriteTimeUtc);
+        }
+
+        [Serializable]
+        private sealed class PluginFileOriginData : PluginOriginData
+        {
+            /// <summary>
+            /// The time the file was created.
+            /// </summary>
+            private readonly DateTimeOffset _creationTimeUtc;
+
+            /// <summary>
+            /// The file path for the plugin file.
+            /// </summary>
+            private readonly string _filePath;
+
+            /// <summary>
+            /// The last time, in coordinated universal time (UTC), that the plugin file was modified.
+            /// </summary>
+            private readonly DateTimeOffset _lastWriteTimeUtc;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="PluginFileOriginData"/> class.
+            /// </summary>
+            /// <param name="path">The full path to the plugin file.</param>
+            /// <param name="creationTimeUtc">The date and time the plugin file was created in UTC time.</param>
+            /// <param name="lastWriteTimeUtc">The date and time the plugin file was last changed in UTC time.</param>
+            public PluginFileOriginData(string path, DateTimeOffset creationTimeUtc, DateTimeOffset lastWriteTimeUtc)
+            {
+                _filePath = path;
+                _creationTimeUtc = creationTimeUtc;
+                _lastWriteTimeUtc = lastWriteTimeUtc;
+            }
+
+            /// <summary>
+            /// Compares the current instance with another object of the same type and returns an integer that
+            /// indicates whether the current instance precedes, follows, or occurs in the same position in the
+            /// sort order as the other object.
+            /// </summary>
+            /// <param name="other">The value of the object with which the current object is being compared.</param>
+            /// <returns>
+            /// A 32-bit signed integer that indicates the relative order of the objects being compared.
+            /// The return value has these meanings:
+            /// Value
+            /// Meaning
+            /// Less than zero
+            /// The current instance is less than <paramref name="other"/>.
+            /// Zero
+            /// The current instance is equal to <paramref name="other"/>.
+            /// Greater than zero
+            /// The current instance is greater than <paramref name="other"/>.
+            /// </returns>
+            protected override int CompareValues(PluginOriginData other)
+            {
+                // We don't strictly need to use the ReferenceEquals method but
+                // it seems more consistent to use it.
+                if (ReferenceEquals(other, null))
+                {
+                    return 1;
+                }
+
+                // Check if other is a null reference by using ReferenceEquals because
+                // we overload the == operator. If other isn't actually null then
+                // we get an infinite loop where we're constantly trying to compare to null.
+                var data = other as PluginFileOriginData;
+                if (ReferenceEquals(data, null))
+                {
+                    throw new ArgumentException(
+                        string.Format(
+                            CultureInfo.InvariantCulture,
+                            Resources.Exceptions_Messages_CompareArgument_WithTypes,
+                            other.GetType().FullName,
+                            GetType().FullName),
+                        @"other");
+                }
+
+                var result = string.Compare(FilePath, data.FilePath, StringComparison.OrdinalIgnoreCase);
+                if (result == 0)
+                {
+                    result = CreationTimeUtc.CompareTo(data.CreationTimeUtc);
+                    if (result == 0)
+                    {
+                        result = LastWriteTimeUtc.CompareTo(data.LastWriteTimeUtc);
+                    }
+                }
+
+                return result;
+            }
+
+            /// <summary>
+            /// Gets the time the file was created.
+            /// </summary>
+            public DateTimeOffset CreationTimeUtc
+            {
+                get
+                {
+                    return _creationTimeUtc;
+                }
+            }
+
+            /// <summary>
+            /// Determines whether the specified <see cref="object"/> is equal to this instance.
+            /// </summary>
+            /// <param name="obj">The <see cref="object"/> to compare with this instance.</param>
+            /// <returns>
+            ///     <see langword="true"/> if the specified <see cref="object"/> is equal to this instance;
+            ///     otherwise, <see langword="false"/>.
+            /// </returns>
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(this, obj))
+                {
+                    return true;
+                }
+
+                var data = obj as PluginFileOriginData;
+                return Equals(data);
+            }
+
+            /// <summary>
+            /// Determines whether the specified <see cref="PluginOriginData"/> is equal to this instance.
+            /// </summary>
+            /// <param name="other">The <see cref="PluginOriginData"/> to compare with this instance.</param>
+            /// <returns>
+            ///     <see langword="true"/> if the specified <see cref="PluginOriginData"/> is equal to this instance;
+            ///     otherwise, <see langword="false"/>.
+            /// </returns>
+            public override bool Equals(PluginOriginData other)
+            {
+                var data = other as PluginFileOriginData;
+
+                // Check if other is a null reference by using ReferenceEquals because
+                // we overload the == operator. If other isn't actually null then
+                // we get an infinite loop where we're constantly trying to compare to null.
+                return !ReferenceEquals(data, null)
+                    && string.Equals(FilePath, data.FilePath, StringComparison.OrdinalIgnoreCase)
+                    && CreationTimeUtc.Equals(data.CreationTimeUtc)
+                    && LastWriteTimeUtc.Equals(data.LastWriteTimeUtc);
+            }
+
+            /// <summary>
+            /// Gets the full file path for the file that contains the plugins.
+            /// </summary>
+            public string FilePath
+            {
+                get
+                {
+                    return _filePath;
+                }
+            }
+
+            /// <summary>
+            /// Returns a hash code for this instance.
+            /// </summary>
+            /// <returns>
+            /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+            /// </returns>
+            public override int GetHashCode()
+            {
+                // As obtained from the Jon Skeet answer to:
+                // http://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
+                // And adapted towards the Modified Bernstein (shown here: http://eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx)
+                //
+                // Overflow is fine, just wrap
+                unchecked
+                {
+                    // Pick a random prime number
+                    int hash = 17;
+
+                    // Mash the hash together with yet another random prime number
+                    hash = (hash * 23) ^ _creationTimeUtc.GetHashCode();
+                    hash = (hash * 23) ^ _filePath.GetHashCode();
+                    hash = (hash * 23) ^ _lastWriteTimeUtc.GetHashCode();
+
+                    return hash;
+                }
+            }
+
+            /// <summary>
+            /// Gets the last time the file that contains the plugins was written to.
+            /// </summary>
+            public DateTimeOffset LastWriteTimeUtc
+            {
+                get
+                {
+                    return _lastWriteTimeUtc;
+                }
+            }
+
+            /// <summary>
+            /// Returns a <see cref="string"/> that represents this instance.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="string"/> that represents this instance.
+            /// </returns>
+            public override string ToString()
+            {
+                return string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0} (created: {1}, last modified {2})",
+                    _filePath,
+                    _creationTimeUtc,
+                    _lastWriteTimeUtc);
             }
         }
     }
---
+++ src/Test.Unit.Plugins.Discovery/Origin/FileSystem/PluginFileOriginTest.cs
@@ -0,0 +1,418 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Nuclei.Nunit.Extensions;
+using NUnit.Framework;
+
+namespace Nuclei.Plugins.Discovery.Origin.FileSystem
+{
+    [TestFixture]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public sealed class PluginFileOriginTest : EqualityContractVerifierTest
+    {
+        private readonly PluginFileOriginHashcodeContractVerfier _hashCodeVerifier = new PluginFileOriginHashcodeContractVerfier();
+
+        private readonly PluginFileOriginEqualityContractVerifier _equalityVerifier = new PluginFileOriginEqualityContractVerifier();
+
+        protected override HashCodeContractVerifier HashContract
+        {
+            get
+            {
+                return _hashCodeVerifier;
+            }
+        }
+
+        protected override IEqualityContractVerifier EqualityContract
+        {
+            get
+            {
+                return _equalityVerifier;
+            }
+        }
+
+        [Test]
+        public void Clone()
+        {
+            PluginFileOrigin first = new PluginFileOrigin("a");
+            PluginFileOrigin second = (PluginFileOrigin)first.Clone();
+
+            Assert.AreEqual(first, second);
+        }
+
+        [Test]
+        public void CompareToOperatorWithEqualObjects()
+        {
+            var first = new PluginFileOrigin("a");
+            object second = (PluginFileOrigin)first.Clone();
+
+            Assert.AreEqual(0, first.CompareTo(second));
+        }
+
+        [Test]
+        public void CompareToWithLargerFirstObjectBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now + TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first.CompareTo(second) > 0);
+        }
+
+        [Test]
+        public void CompareToWithLargerFirstObjectBasedOnModifiedTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now + TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first.CompareTo(second) > 0);
+        }
+
+        [Test]
+        public void CompareToWithLargerFirstObjectBasedOnPath()
+        {
+            var first = new PluginFileOrigin("b");
+            var second = new PluginFileOrigin("a");
+
+            Assert.IsTrue(first.CompareTo(second) > 0);
+        }
+
+        [Test]
+        public void CompareToWithNullObject()
+        {
+            PluginFileOrigin first = new PluginFileOrigin("a");
+            object second = null;
+
+            Assert.AreEqual(1, first.CompareTo(second));
+        }
+
+        [Test]
+        public void CompareToWithSmallerFirstObjectBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now - TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first.CompareTo(second) < 0);
+        }
+
+        [Test]
+        public void CompareToWithSmallerFirstObjectBasedOnModifiedTimed()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now - TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first.CompareTo(second) < 0);
+        }
+
+        [Test]
+        public void CompareToWithSmallerFirstObjectBasedOnPath()
+        {
+            var first = new PluginFileOrigin("a");
+            var second = new PluginFileOrigin("b");
+
+            Assert.IsTrue(first.CompareTo(second) < 0);
+        }
+
+        [Test]
+        public void CompareToWithUnequalObjectTypes()
+        {
+            PluginFileOrigin first = new PluginFileOrigin("a");
+            object second = new object();
+
+            Assert.Throws<ArgumentException>(() => first.CompareTo(second));
+        }
+
+        [Test]
+        public void Create()
+        {
+            var path = "a";
+            var now = DateTimeOffset.Now;
+            var creation = now;
+            var modified = now + TimeSpan.FromHours(2);
+            var origin = new PluginFileOrigin(path, creation, modified);
+
+            Assert.IsNotNull(origin);
+            Assert.AreEqual(path, origin.FilePath);
+            Assert.AreEqual(creation, origin.CreationTimeUtc);
+            Assert.AreEqual(modified, origin.LastWriteTimeUtc);
+        }
+
+        [Test]
+        [SuppressMessage(
+            "Microsoft.Usage",
+            "CA1806:DoNotIgnoreMethodResults",
+            MessageId = "Nuclei.Plugins.Discovery.Origin.FileSystem.PluginFileOrigin",
+            Justification = "Testing that the constructor throws an exception.")]
+        public void CreateWithEmptyFilePath()
+        {
+            Assert.Throws<ArgumentException>(() => new PluginFileOrigin(string.Empty));
+        }
+
+        [Test]
+        [SuppressMessage(
+            "Microsoft.Usage",
+            "CA1806:DoNotIgnoreMethodResults",
+            MessageId = "Nuclei.Plugins.Discovery.Origin.FileSystem.PluginFileOrigin",
+            Justification = "Testing that the constructor throws an exception.")]
+        public void CreateWithNullFilePath()
+        {
+            Assert.Throws<ArgumentException>(() => new PluginFileOrigin(null));
+        }
+
+        [Test]
+        public void LargerThanOperatorWithBothObjectsNull()
+        {
+            PluginFileOrigin first = null;
+            PluginFileOrigin second = null;
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithEqualObjects()
+        {
+            var first = new PluginFileOrigin("a");
+            var second = (PluginFileOrigin)first.Clone();
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectLargerBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now + TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectLargerBasedOnModifiedTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now + TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectLargerBasedOnPath()
+        {
+            var first = new PluginFileOrigin("b");
+            var second = new PluginFileOrigin("a");
+
+            Assert.IsTrue(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectNull()
+        {
+            PluginFileOrigin first = null;
+            PluginFileOrigin second = new PluginFileOrigin("a");
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectSmallerBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now - TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectSmallerBasedOnModifiedTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now - TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithFirstObjectSmallerBasedOnPath()
+        {
+            var first = new PluginFileOrigin("a");
+            var second = new PluginFileOrigin("b");
+
+            Assert.IsFalse(first > second);
+        }
+
+        [Test]
+        public void LargerThanOperatorWithSecondObjectNull()
+        {
+            PluginFileOrigin first = new PluginFileOrigin("a");
+            PluginFileOrigin second = null;
+
+            Assert.IsTrue(first > second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithBothObjectsNull()
+        {
+            PluginFileOrigin first = null;
+            PluginFileOrigin second = null;
+
+            Assert.IsFalse(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithEqualObjects()
+        {
+            var first = new PluginFileOrigin("a");
+            var second = (PluginFileOrigin)first.Clone();
+
+            Assert.IsFalse(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectLargerBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now + TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsFalse(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectLargerBasedOnModifiedTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now + TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsFalse(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectLargerBasedOnPath()
+        {
+            var first = new PluginFileOrigin("b");
+            var second = new PluginFileOrigin("a");
+
+            Assert.IsFalse(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectNull()
+        {
+            PluginFileOrigin first = null;
+            PluginFileOrigin second = new PluginFileOrigin("a");
+
+            Assert.IsTrue(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectSmallerBasedOnCreationTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now - TimeSpan.FromHours(2), now);
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectSmallerBasedOnModifiedTime()
+        {
+            var now = DateTimeOffset.Now;
+            var first = new PluginFileOrigin("a", now, now - TimeSpan.FromHours(2));
+            var second = new PluginFileOrigin("a", now, now);
+
+            Assert.IsTrue(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithFirstObjectSmallerBasedOnPath()
+        {
+            var first = new PluginFileOrigin("a");
+            var second = new PluginFileOrigin("b");
+
+            Assert.IsTrue(first < second);
+        }
+
+        [Test]
+        public void SmallerThanOperatorWithSecondObjectNull()
+        {
+            PluginFileOrigin first = new PluginFileOrigin("a");
+            PluginFileOrigin second = null;
+
+            Assert.IsFalse(first < second);
+        }
+
+        private sealed class PluginFileOriginEqualityContractVerifier : EqualityContractVerifier<PluginFileOrigin>
+        {
+            private readonly PluginFileOrigin _first = new PluginFileOrigin("a");
+
+            private readonly PluginFileOrigin _second = new PluginFileOrigin("b");
+
+            protected override PluginFileOrigin Copy(PluginFileOrigin original)
+            {
+                return (PluginFileOrigin)original.Clone();
+            }
+
+            protected override PluginFileOrigin FirstInstance
+            {
+                get
+                {
+                    return _first;
+                }
+            }
+
+            protected override PluginFileOrigin SecondInstance
+            {
+                get
+                {
+                    return _second;
+                }
+            }
+
+            protected override bool HasOperatorOverloads
+            {
+                get
+                {
+                    return true;
+                }
+            }
+        }
+
+        private sealed class PluginFileOriginHashcodeContractVerfier : HashCodeContractVerifier
+        {
+            private readonly IEnumerable<PluginFileOrigin> _distinctInstances
+                = new List<PluginFileOrigin>
+                     {
+                        new PluginFileOrigin("a"),
+                        new PluginFileOrigin("b"),
+                        new PluginFileOrigin("c"),
+                        new PluginFileOrigin("d"),
+                        new PluginFileOrigin("e"),
+                        new PluginFileOrigin("f"),
+                     };
+
+            protected override IEnumerable<int> GetHashCodes()
+            {
+                return _distinctInstances.Select(i => i.GetHashCode());
+            }
+        }
+    }
+}
