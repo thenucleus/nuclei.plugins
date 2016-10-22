9cccd84c40f456e014512c71f3e1ec94c9217ff8
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-10-23 00:07:44 +1300

    Updating the plugin detector
    references #2

--- src/Nuclei.Plugins.Discovery/Container/AssemblyPluginDetector.cs
+++ src/Nuclei.Plugins.Discovery/Container/AssemblyPluginDetector.cs
@@ -7,32 +7,21 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.IO.Abstractions;
 using System.Linq;
-using Nuclei.Diagnostics;
-using Nuclei.Diagnostics.Logging;
 using Nuclei.Plugins.Core;
-using Nuclei.Plugins.Discovery.Properties;
+using Nuclei.Plugins.Discovery.Origin.FileSystem;
 
-namespace Nuclei.Plugins.Discovery.Container.Assembly
+namespace Nuclei.Plugins.Discovery.Container
 {
     /// <summary>
     /// Provides the methods to find and scan plugins available in assembly files.
     /// </summary>
     public sealed class AssemblyPluginDetector : IProcessPluginOriginChanges
     {
-        /// <summary>
-        /// The objects that provides the diagnostics methods for the application.
-        /// </summary>
-        private readonly SystemDiagnostics _diagnostics;
-
-        /// <summary>
-        /// The collection containing all listeners.
-        /// </summary>
-        private readonly List<IPluginListener> _listeners
-            = new List<IPluginListener>();
+        private static readonly IPluginType[] _acceptedPluginTypes = new[]
+            {
+                new FilePluginType("dll"),
+            };
 
         /// <summary>
         /// The object that stores information about all the parts and the part groups.
@@ -49,35 +38,20 @@ namespace Nuclei.Plugins.Discovery.Container.Assembly
         /// Initializes a new instance of the <see cref="AssemblyPluginDetector"/> class.
         /// </summary>
         /// <param name="repository">The object that stores information about all the parts and the part groups.</param>
-        /// <param name="listeners">The collection of object that detect changes to plugin files.</param>
         /// <param name="scannerBuilder">The function that is used to create an assembly scanner.</param>
-        /// <param name="diagnostics">The object that provides the diagnostics methods for the application.</param>
         /// <exception cref="ArgumentNullException">
         ///     Thrown if <paramref name="repository"/> is <see langword="null" />.
         /// </exception>
         /// <exception cref="ArgumentNullException">
-        ///     Thrown if <paramref name="listeners"/> is <see langword="null" />.
-        /// </exception>
-        /// <exception cref="ArgumentNullException">
         ///     Thrown if <paramref name="scannerBuilder"/> is <see langword="null" />.
-        /// </exception>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown if <paramref name="diagnostics"/> is <see langword="null" />.
         /// </exception>
         public AssemblyPluginDetector(
             IPluginRepository repository,
-            IEnumerable<IPluginListener> listeners,
-            Func<IPluginRepository, IAssemblyScanner> scannerBuilder,
-            SystemDiagnostics diagnostics)
+            Func<IPluginRepository, IAssemblyScanner> scannerBuilder)
         {
             if (repository == null)
             {
                 throw new ArgumentNullException("repository");
-            }
-
-            if (listeners == null)
-            {
-                throw new ArgumentNullException("listeners");
             }
 
             if (scannerBuilder == null)
@@ -85,51 +59,40 @@ namespace Nuclei.Plugins.Discovery.Container.Assembly
                 throw new ArgumentNullException("scannerBuilder");
             }
 
-            if (diagnostics == null)
-            {
-                throw new ArgumentNullException("diagnostics");
-            }
-
-            _diagnostics = diagnostics;
             _repository = repository;
             _scannerBuilder = scannerBuilder;
+        }
 
-            foreach (var listener in listeners)
+        /// <summary>
+        /// Gets a collection that describes what types of plugins are accepted by the processor.
+        /// </summary>
+        public IEnumerable<IPluginType> AcceptedPluginTypes
+        {
+            get
             {
-                _listeners.Add(listener);
-                listener.OnPluginDetected += HandlePluginDetected;
+                return _acceptedPluginTypes;
             }
         }
 
-        private void HandlePluginDetected(object sender, PluginFoundEventArgs e)
+        /// <summary>
+        /// Processes the added files.
+        /// </summary>
+        /// <param name="newPlugins">The collection that contains the names of all the new plugins.</param>
+        public void Added(params PluginOrigin[] newPlugins)
         {
-            var knownFiles = _repository.KnownPluginOrigins().OfType<PluginFileOrigin>();
-
-            var changedKnownFiles = knownFiles
-                .Where(p => files.Any(f => string.Equals(p.FilePath, f, StringComparison.OrdinalIgnoreCase)))
-                .Where(p => _fileSystem.File.GetLastWriteTimeUtc(p.FilePath) > p.LastWriteTimeUtc)
-                .Select(p => p.FilePath);
-
-            var changedFilePaths = new HashSet<string>(files);
-            changedFilePaths.SymmetricExceptWith(knownFiles.Select(p => p.FilePath));
-
-            var newFiles = changedFilePaths.Where(file => _fileSystem.File.Exists(file));
-
-            RemoveDeletedPlugins(changedFilePaths);
-            StorePlugins(changedKnownFiles.Concat(newFiles));
-
-            _diagnostics.Log(
-                LevelToLog.Info,
-                string.Format(
-                    CultureInfo.InvariantCulture,
-                    Resources.Plugins_LogMessage_Detector_FileScanCompleted,
-                    directory));
+            var filesToAdd = newPlugins
+                .OfType<PluginFileOrigin>()
+                .Select(f => f.FilePath);
+            StorePlugins(filesToAdd);
         }
 
-        private void RemoveDeletedPlugins(IEnumerable<string> changedFilePaths)
+        /// <summary>
+        /// Processes the removed files.
+        /// </summary>
+        /// <param name="removedPlugins">The collection that contains the names of all the plugins that were removed.</param>
+        public void Removed(params PluginOrigin[] removedPlugins)
         {
-            var deletedFiles = changedFilePaths.Where(file => !_fileSystem.File.Exists(file)).Select(file => new PluginFileOrigin(file));
-            _repository.RemovePlugins(deletedFiles);
+            _repository.RemovePlugins(removedPlugins);
         }
 
         private void StorePlugins(IEnumerable<string> filesToScan)
---
+++ src/Test.Unit.Plugins.Discovery/Container/AssemblyPluginDetectorTest.cs
@@ -0,0 +1,102 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Moq;
+using Nuclei.Plugins.Core;
+using Nuclei.Plugins.Discovery.Origin.FileSystem;
+using NUnit.Framework;
+
+namespace Nuclei.Plugins.Discovery.Container
+{
+    [TestFixture]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public sealed class AssemblyPluginDetectorTest
+    {
+        private sealed class MockScanner : IAssemblyScanner
+        {
+            private IEnumerable<string> _files;
+
+            public void Scan(IEnumerable<string> assemblyFilesToScan)
+            {
+                _files = assemblyFilesToScan;
+            }
+
+            public IEnumerable<string> FilesToScan
+            {
+                get
+                {
+                    return _files;
+                }
+            }
+        }
+
+        [Test]
+        public void Added()
+        {
+            var repository = new Mock<IPluginRepository>();
+            {
+                repository.Setup(r => r.KnownPluginOrigins())
+                    .Returns(Enumerable.Empty<PluginFileOrigin>());
+            }
+
+            var files = new List<PluginFileOrigin>
+                {
+                    new PluginFileOrigin(@"c:\temp\foobar.dll", DateTimeOffset.Now, DateTimeOffset.Now),
+                    new PluginFileOrigin(@"c:\temp\foobar2.dll", DateTimeOffset.Now.AddHours(-2), DateTimeOffset.Now),
+                };
+
+            var scanner = new MockScanner();
+            Func<IPluginRepository, IAssemblyScanner> scannerBuilder = r => scanner;
+
+            var detector = new AssemblyPluginDetector(
+                repository.Object,
+                scannerBuilder);
+
+            var origins = files.Cast<PluginOrigin>().ToArray();
+            detector.Added(origins.ToArray());
+            Assert.That(scanner.FilesToScan, Is.EquivalentTo(files));
+        }
+
+        [Test]
+        public void Removed()
+        {
+            var files = new List<PluginFileOrigin>
+                {
+                    new PluginFileOrigin(@"c:\temp\foobar.dll", DateTimeOffset.Now, DateTimeOffset.Now),
+                    new PluginFileOrigin(@"c:\temp\foobar2.dll", DateTimeOffset.Now.AddHours(-2), DateTimeOffset.Now),
+                };
+
+            IEnumerable<PluginOrigin> removedPlugins = null;
+            var repository = new Mock<IPluginRepository>();
+            {
+                repository.Setup(r => r.KnownPluginOrigins())
+                    .Returns(files);
+                repository.Setup(r => r.RemovePlugins(It.IsAny<IEnumerable<PluginOrigin>>()))
+                    .Callback<IEnumerable<PluginOrigin>>(i => removedPlugins = i);
+            }
+
+            var scanner = new MockScanner();
+            Func<IPluginRepository, IAssemblyScanner> scannerBuilder = r => scanner;
+
+            var detector = new AssemblyPluginDetector(
+                repository.Object,
+                scannerBuilder);
+
+            var origins = files.Cast<PluginOrigin>().ToArray();
+            detector.Removed(origins);
+            Assert.IsNotNull(removedPlugins);
+            Assert.AreSame(origins, removedPlugins);
+        }
+    }
+}
