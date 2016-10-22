beb8da3edd6ca2a2af42dda164a54375daaf84a9
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-10-23 00:08:25 +1300

    Adding the NuGet projects
    references #2

---
+++ src/Nuclei.Plugins.Core.NuGet/IInstallPackages.cs
@@ -0,0 +1,37 @@
+﻿//--------------------------------------------;---------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Client;
+using NuGet.Packaging.Core;
+
+namespace Nuclei.Plugins.Core.NuGet
+{
+    /// <summary>
+    /// Defines the interface for objects that install NuGet packages from a given feed.
+    /// </summary>
+    public interface IInstallPackages
+    {
+        /// <summary>
+        /// Installs a given version of a package and its dependencies.
+        /// </summary>
+        /// <param name="name">The ID of the package.</param>
+        /// <param name="outputLocation">The full path of the directory where the packages should be installed.</param>
+        /// <param name="postInstallAction">
+        /// An action that is run after each package is installed. The input values are the <paramref name="outputLocation"/>,
+        /// the path to the installed package and the package ID.
+        /// </param>
+        void Install(
+            PackageIdentity name,
+            string outputLocation,
+            Action<string, string, PackageIdentity> postInstallAction = null);
+    }
+}
---
+++ src/Test.Unit.Plugins.Core.NuGet/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿//--------------------------------------------;---------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+
+namespace Test.Unit.Plugins.Core.NuGet
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
