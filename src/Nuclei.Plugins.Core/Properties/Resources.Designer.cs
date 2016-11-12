ec201af0bc2257b31a15c2edff96f20deaee1112
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-11-12 23:22:34 +1300

    Missed the resource update

--- src/Nuclei.Plugins.Core/Properties/Resources.Designer.cs
+++ src/Nuclei.Plugins.Core/Properties/Resources.Designer.cs
@@ -97,6 +97,15 @@ namespace Nuclei.Plugins.Core.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The specified plugin origin is not valid for the current type loader..
+        /// </summary>
+        internal static string Exceptions_Messages_InvalidPluginOrigin {
+            get {
+                return ResourceManager.GetString("Exceptions_Messages_InvalidPluginOrigin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The parameter should not be an empty string..
         /// </summary>
         internal static string Exceptions_Messages_ParameterShouldNotBeAnEmptyString {
